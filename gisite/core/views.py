4b27e00e8d2cdb4151a9a68990b0cd29060f4777
Wookyz <marcoscardoso.programmer@gmail.com>
2021-03-16 20:59:34 -0300

    Base.html Finished

--- gisite/core/views.py
+++ gisite/core/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    context = {
+        'title': "Home",
+    }
+    return render(request, 'core/index.html', context=context)
