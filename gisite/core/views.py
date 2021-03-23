be8a6a41637f201ae9098bfdb2f80aeae7308206
Wookyz <marcoscardoso.programmer@gmail.com>
2021-03-23 19:27:58 -0300

    Create builds page

--- gisite/core/views.py
+++ gisite/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+
+
+API_GENSHIN = "https://api.genshin.dev"
 
 # Create your views here.
 def index(request):
@@ -8,3 +12,14 @@ def index(request):
         'title': "Home",
     }
     return render(request, 'core/index.html', context=context)
+
+
+def builds(request):
+    characters = requests.get(API_GENSHIN + "/characters/")
+    characters = characters.json()
+    context = {
+        'title': "Builds",
+        'characters': characters,
+        'url_api': API_GENSHIN+"/characters",
+    }
+    return render(request, 'core/builds.html', context=context)
